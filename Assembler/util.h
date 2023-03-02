39d8eea2263a5212e1c543e164544049c372a747
ammaarahmad1999 <ammaarahmad1999@gmail.com>
2023-03-02 08:43:43 +0530

    Assembler and Emulator Bifurcated

---
+++ Assembler/util.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Util{
+    public:
+        static bool isValidLabel(string label);
+        static string trim(string token);
+        static string AddPadding(string binary);
+};
+
+#endif
